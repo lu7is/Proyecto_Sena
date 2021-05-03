ddcc79ab11bb394e56e8591ab0f006a2dee262f1
yaatuesta <aatuesta00@hotmail.com>
2021-05-02 20:04:20 -0500

    Database connection

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'key' => env('APP_KEY'),
-        'cipher' => env('APP_KEY'),
+        'cipher' => 'AES-256-CBC'
 
     ],
 
